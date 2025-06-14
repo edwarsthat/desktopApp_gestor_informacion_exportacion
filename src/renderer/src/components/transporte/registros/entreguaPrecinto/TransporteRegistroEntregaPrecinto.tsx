71e7fbbcd08ac1deee32e0c3a0afd14ef5d8a3f4
CelifrutD <transformaciondigital@celifrut.com>
2025-06-14 12:50:39 -0500

    se termino los test de ingreso entrega precinto2

--- src/renderer/src/components/transporte/registros/entreguaPrecinto/TransporteRegistroEntregaPrecinto.tsx
+++ src/renderer/src/components/transporte/registros/entreguaPrecinto/TransporteRegistroEntregaPrecinto.tsx
@@ -1,6 +1,41 @@
 /* eslint-disable prettier/prettier */
 
+import useAppContext from "@renderer/hooks/useAppContext"
+import { useFetchPaginatedList } from "@renderer/hooks/useFetchPaginatedList"
+import { useEffect, useState } from "react"
+
 export default function TransporteRegistroEntregaPrecinto(): JSX.Element {
+    const {setLoading, loading} = useAppContext();
+    const [ page, setPage] = useState<number>(1)
+    const {obtenerCantidadElementos, obtenerData, data} = useFetchPaginatedList({
+        actionData:"get_transporte_registros_entregaPrecintos",
+        actionNumberData:"get_transporte_registros_entregaPrecintos_numeroElementos",
+        page:page
+    })
+    useEffect(()=>{
+        const fetchData = async (): Promise<void> => {
+            try{
+                setLoading(true);
+                await obtenerCantidadElementos();
+                await obtenerData();
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData();
+    },[])
+    useEffect(()=>{
+        try{
+        setLoading(true);
+        obtenerData();
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        } finally {
+            setLoading(false)
+        }
+    },[page])
     return (
         <div>
             <div className="navBar"></div>
